121636503c913f2c92225fd0d53b4aa4bf2d987d
darcystruble <darcyhallstruble@gmail.com>
2023-11-03 12:26:16 -0400

    added Main component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import Main from './components/Main'
 import './App.css'
 
 function App() {
 
   return (
       <div>
+        <Main />
       </div>
      
   )
