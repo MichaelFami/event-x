2112836e1ac04049ff476b763c2da1b885494de4
darcystruble <darcyhallstruble@gmail.com>
2023-11-05 11:18:55 -0500

    updates to calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,10 +4,19 @@ import './Calendar.css'
 
 export default function CalendarElement () {
     const [value, setValue] = useState(new Date())
+    // const datesToAddClassTo = [tomorrow, in3Days, in5Days]
 
     function handleChange(nextValue) {
         setValue(nextValue)
     }
+
+    // function titleClassName({ date, view }) {
+    //     if (view === 'month'){
+    //         if (datesToAddClassTo.find((dDate) => isSameDay(dDate, date))) {
+    //             return 'selectedDate'
+    //         }
+    //     }
+    // }
     return (
         <div className='calendar-outer'>
             <Calendar onChange={handleChange} value={value} />
