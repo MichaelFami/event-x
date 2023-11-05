f066212b3f56efe4bf6e455f0fbba91b96eaa0cd
darcystruble <darcyhallstruble@gmail.com>
2023-11-05 11:54:59 -0500

    created a first draft flow for user page

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,7 +1,21 @@
-//imports here
+import Card from 'react-bootstrap/Card'
 
 export default function EventList () {
     return (
-        <div></div>
+        <div>
+            <h1>Upcoming Events</h1>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom"
+import CalendarElement from "./Calendar"
+
 export default function User () {
     return (
-        <div></div>
+        <div>
+            <h1 className="title">User Name</h1>
+            <CalendarElement />
+            <div className="user-more-info">
+            <Link to="/eventfind" className="links"><h3>Find Event</h3></Link>
+                <Link to="/eventlist" className="links"><h3>UpComing Events</h3></Link>
+                <ul>
+                    <li>Upcoming event-clickable</li>
+                    <li>Upcoming event-clickable</li>
+                    <li>Upcoming event-clickable</li>
+                </ul>
+                <Link className="links"><h3>Groups</h3></Link>
+                <ul>
+                    <li>Group Name-clickable</li>
+                    <li>Group Name-clickable</li>
+                    <li>Group Name-clickable</li>
+                </ul>
+            </div>
+        </div>
     )
 }
