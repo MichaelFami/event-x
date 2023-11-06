eb90589aae77ed571f433bee3ea22927044a61e7
darcystruble <darcyhallstruble@gmail.com>
2023-11-06 15:36:50 -0500

    updates to scss, css, addevent and event

--- src/components/AddEvent.jsx
+++ src/components/AddEvent.jsx
@@ -1,52 +1,78 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
 export default function AddEvent () {
+    const stateAbbrev = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+
+    const initialState = {date: '', name: '', address: '', address2: '', city: '', state: '', zip: '', description: '', capacity: ''}
+    const [formState, setFormState] = useState(initialState)
+    const navigate = useNavigate()
+
+    const handleChange = e => {
+        // console.log(e.target.id, e.target.value)
+        setFormState({...formState, [e.target.id]: e.target.value})
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(formState)
+        navigate('/event/:id')
+    }
+
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Event name</Form.Label>
-                <Form.Control type="event" placeholder="Enter Event Name" />
+                <Form.Control type="event" placeholder="Enter Event Name" onChange={handleChange}/>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formGridAddress1">
+            <Form.Group className="mb-3" controlId="address">
                 <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
+            <Form.Control placeholder="1234 Main St" onChange={handleChange}/>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formGridAddress2">
+            <Form.Group className="mb-3" controlId="address2">
                 <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
+                <Form.Control placeholder="Apartment, studio, or floor" onChange={handleChange}/>
             </Form.Group>
 
             <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridCity">
+                <Form.Group as={Col} controlId="city">
                 <Form.Label>City</Form.Label>
-                <Form.Control />
+                <Form.Control onChange={handleChange}/>
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridState">
+                <Form.Group as={Col} controlId="state">
                     <Form.Label>State</Form.Label>
-                    <Form.Select defaultValue="Choose...">
+                    <Form.Select defaultValue="Choose..." onChange={handleChange}>
                         <option>Choose...</option>
-                        <option>...</option>
+                        {stateAbbrev.map((abb) => (
+                          <option key={abb}>{abb}</option>  
+                        ))}
                     </Form.Select>
                 </Form.Group>
-                <Form.Group as={Col} controlId="formGridZip">
+                <Form.Group as={Col} controlId="zip">
                     <Form.Label>Zip</Form.Label>
-                    <Form.Control />
+                    <Form.Control onChange={handleChange} />
                 </Form.Group>
             </Row>
-            <Form.Group className="mb-3" controlId="formDescription">
+            <Form.Group className="mb-3" controlId="description">
                 <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" type="discription" placeholder="Enter Event Name" />
+                <Form.Control as="textarea" type="description" placeholder="Enter Event Name" onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="capacity">
+                <Form.Label>Max Capacity</Form.Label>
+            <Form.Control placeholder="#" onChange={handleChange} />
             </Form.Group>
   
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+
+            <Button variant="primary" type="submit">
+           Submit
+            </Button>
+        </Form>
     )
 }
